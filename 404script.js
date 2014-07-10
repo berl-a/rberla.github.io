859391c41947da53b398d9de1779ed95b60ed8af
Roman Berla <rberla@ya.ru>
2014-07-10 19:35:36 +0300

    Added 404 page

---
+++ 404script.js
@@ -0,0 +1,36 @@
+$(document).ready(
+	function(){
+		
+		var number_of_clicked_letters = 0;
+		
+		$('img:not(div>img)').hover(
+			function() {
+				$(this).addClass('filtered');
+			}, 
+			function() {
+				$(this).removeClass('filtered');
+			}
+		);
+		
+		$.ionSound(
+			{
+				sounds: [
+					"clap"
+				],
+				path: "plugins/ion-sound/",
+				volume: 0.7
+			}	
+		);
+		
+		$('p span.unusual').click(
+			function() {
+				$(this).removeClass('unusual');
+				number_of_clicked_letters++;
+				
+				if(number_of_clicked_letters === 5) {
+					$.ionSound.play("clap");
+				}
+			}
+		);
+	}
+);
